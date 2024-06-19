08baa0cd201f56a579ec5cae96da4adb591521bf
Riccardo Reho <riccardoreho95@gmail.com>
2024-06-19 07:09:53 +0200

    not sure

--- yambopy/wannier/wann_par_utils.py
+++ yambopy/wannier/wann_par_utils.py
@@ -11,22 +11,19 @@ def get_exc_overlap_ttp_par(argslist):
     iqpb = kmpgrid.qpb_grid_table[iq, ib][1]
 
     for it in range(dimbse):
-        eigvec_ic = eigvec[:, ic]
-        eigvec_icp = eigvec[:, icp]
-        eigvec_iv = eigvec[:, iv]
-        eigvec_ivp = eigvec[:, ivp]
-
         for itp in range(dimbse):
-            ikmq = kmpgrid.kmq_grid_table[ik, iq][1]
+            ik = BSE_table[l][0]
+            iv = BSE_table[l][1] 
+            ic = BSE_table[l][2] 
+            ikp = BSE_table[lp][0]
+            ivp = BSE_table[lp][1] 
+            icp = BSE_table[lp][2] 
+            iqpb = kmpgrid.qpb_grid_table[iq, ib][1]
+            ikmq = kmpgrid.kmq_grid_table[ik,iq][1]
             ikpbover2 = kmpgrid.kpbover2_grid_table[ik, ib][1]
             ikmqmbover2 = kmpgrid.kmqmbover2_grid_table[ik, iq, ib][1]
-
-            conj_term = np.conjugate(h2peigvec_vck[ikq, l, bse_nv-nv+iv, ic-nv, ik])
-            eigvec_term = h2peigvec_vck[iqpb, lp, bse_nv-nv+ivp, icp-nv, ikpbover2]
-            dot_product1 = np.vdot(eigvec_ic, eigvec_icp)
-            dot_product2 = np.vdot(eigvec_ivp, eigvec_iv)
-
-            Mssp_ttp += conj_term * eigvec_term * dot_product1 * dot_product2
+            Mssp_ttp += np.conjugate(h2peigvec_vck[ikq,l,bse_nv-nv+iv,ic-nv,ik])*h2peigvec_vck[iqpb,lp,bse_nv-nv+ivp,icp-nv, ikpbover2]*\
+                            np.vdot(eigvec[ik,:, ic], eigvec[ikpbover2,:, icp])*np.vdot(eigvec[ikmqmbover2,:,ivp], eigvec[ikmq,:,iv]) 
     return (l, lp, iq, ib, Mssp_ttp)
 
 
