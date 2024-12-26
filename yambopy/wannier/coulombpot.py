fix units in coulombpot

yambopy/wannier/coulombpot.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.special import j0, y0, k0, j1, y1, k1  # Bessel functions from scipy.special
 from yambopy.lattice import replicate_red_kmesh, calculate_distances, car_red,modvec
-from yambopy.units import alpha,ha2ev, ang2bohr
+from yambopy.units import alpha,ha2ev, ang2bohr,bohr2ang
 
 class CoulombPotentials:
     '''
@@ -15,21 +15,21 @@ class CoulombPotentials:
     pi = np.pi
 
     def __init__(self, ngrid, lattice, v0=0.0, lc=15.0, w=1.0, r0=1.0, tolr=0.001, ediel=[1.0,1.0,1.0]):
-        print('''Warning! CoulombPotentials works with atomic units and return energy in eV \n
-                Check consistency of units in the methods, they have not been properly tested
+        print('''Warning! CoulombPotentials works lat vol in angstrom and kpoints in 1/angstroms.
               ''')
         #lattice is an instance of YamboLatticeDb
         self.ngrid = ngrid
         self.lattice = lattice
-        self.rlat = lattice.lat
+        self.rlat = lattice.lat*bohr2ang
         self.tolr = tolr
-        self.dir_vol = lattice.lat_vol  #real lattice volume
-        self.rec_vol = lattice.rlat_vol # reciprocal lattice volume
+        self.dir_vol = lattice.lat_vol*bohr2ang**3  #working in angstrom
+        self.rec_vol = lattice.rlat_vol*2*np.pi*ang2bohr # reciprocal lattice volume in Bohr
         self.ediel = ediel # ediel(1) Top substrate, ediel(2) \eps_d, ediel(3) Bot substrate     
-        self.lc = lc
+        self.lc = lc # in angstrom
         self.w = w
         self.r0 = r0
         self.v0 = v0
+        self.alpha = -0.0904756*10**3
 
     def v2dk(self, kpt1, kpt2):
         pass
@@ -47,7 +47,7 @@ class CoulombPotentials:
         a0 = vsize[0]/2+vsize[1]/2        
         modk = modvec(kpt1, kpt2)
         #compute area of unit cell
-        vc = np.linalg.norm(np.cross(self.rlat[0],self.rlat[1]))        
+        vc = np.linalg.norm(np.cross(self.rlat[0],self.rlat[1]))
 
         r0 = ((ediel[1] - 1.0) * lc) / (ediel[0] + ediel[2])
         vbz = 1.0 / (np.prod(self.ngrid) * vc)
@@ -57,9 +57,9 @@ class CoulombPotentials:
 
         if modk < self.tolr:
             # here factor 2.0*self.pi gets divided by 2*pi
-            v2dk = vbz * (a0 * np.sqrt(gridaux1)/ed ) * (alpha1 + auxi * alpha2 + alpha3 * auxi**2)
+            v2dk = vbz * (self.alpha* a0 * np.sqrt(gridaux1))/(2.*np.pi*ed) * (alpha1 + auxi * alpha2 + alpha3 * auxi**2)
         else:
-            v2dk = vbz * (2.0*self.pi/ed) * (1.0 / (modk * (1.0 + r0 * modk)))
+            v2dk = vbz * (self.alpha/ed) * (1.0 / (modk * (1.0 + r0 * modk)))
 
         return v2dk*ha2ev
 
@@ -70,7 +70,7 @@ class CoulombPotentials:
         if modk < self.tolr:
             vcoul = self.v0
         else:
-            vcoul = vbz * (2*self.pi / ed) * (1.0 / (modk ** 2))
+            vcoul = vbz * (self.alpha / ed) * (1.0 / (modk ** 2))
 
         return vcoul*ha2ev
 
@@ -85,16 +85,16 @@ class CoulombPotentials:
         modk = modvec(kpt1,kpt2)
 
         if gpar < self.tolr and gz < self.tolr:
-            v2dt = (vbz * 2*self.pi) * (1/8.0 * rc * rc)
+            v2dt = (vbz * self.alpha) * (1/2.0 * rc * rc)
         elif gpar < self.tolr and gz >= self.tolr:
-            v2dt = (vbz * 2*self.pi) * (factor / modk**2) * (1.0 - np.cos(gz * rc) - (gz * rc * np.sin(gz * rc)))
+            v2dt = (vbz * self.alpha) * (factor / modk**2) * (1.0 - np.cos(gz * rc) - (gz * rc * np.sin(gz * rc)))
         else:
             aux1 = gz / gpar
             aux2 = gpar * rc
             aux3 = gz * rc
             aux4 = aux1 * np.sin(aux3)
             aux5 = np.cos(aux3)
-            v2dt = (vbz * 2*self.pi) * (factor / modk**2) * (1.0 + (np.exp(-aux2) * (aux4 - aux5)))
+            v2dt = (vbz * 2*self.alpha) * (factor / modk**2) * (1.0 + (np.exp(-aux2) * (aux4 - aux5)))
 
         return v2dt*ha2ev
 
@@ -117,9 +117,9 @@ class CoulombPotentials:
         if modk < self.tolr:
             v2dt2 = 0.0
         else:
-            v2dt2 = (vbz * 2*self.pi) * (factor / modk**2) * (1.0 - np.exp(-0.5 * qxy * lc) * np.cos(0.5 * lc * vkpt[2]))
+            v2dt2 = (vbz * self.alpha) * (factor / modk**2) * (1.0 - np.exp(-0.5 * qxy * lc) * np.cos(0.5 * lc * vkpt[2]))
         
-        return v2dt2*ha2ev
+        return v2dt2
 
     def v2drk(self, kpt1, kpt2):
         '''
@@ -152,7 +152,7 @@ class CoulombPotentials:
 
             ew = (aux1 / aux2) + aux3       #F(Q)
 
-            v2drk = (vbz * 2.0 * self.pi) * np.exp(-modk * w) * (1.0 / ew) * (1.0 / modk)
+            v2drk = (vbz * self.alpha) * np.exp(-modk * w) * (1.0 / ew) * (1.0 / modk)
 
         return v2drk*ha2ev
     
@@ -166,9 +166,9 @@ class CoulombPotentials:
         factor = 1.0
 
         if modk < self.tolr:
-            v0dt = 2*self.pi*vbz*cr**2
+            v0dt = 1/2.*self.alpha*vbz*cr**2
         else:
-            v0dt = 2*self.pi*vbz*factor/(modk**2)*(1-np.cos(cr*modk))
+            v0dt = self.alpha*vbz*factor/(modk**2)*(1-np.cos(cr*modk))
         return v0dt
 
     def v2doh(self, kpt1, kpt2):
@@ -181,41 +181,41 @@ class CoulombPotentials:
         if modk < self.tolr:
             v2doh = 0.0
         else :
-            v2doh = 2*self.pi*vbz*np.exp(-w*modk)*(1.0/(ez*modk))
+            v2doh = self.alpha*vbz*np.exp(-w*modk)*(1.0/(ez*modk))
         return v2doh
     
-    def v2dt_vectorized(self, k_diffs):
-        # this function might be useful to avoid do loops
-        vc = self.dir_vol
-        vbz = 1.0 / (np.prod(self.ngrid) * vc)
-        gz = np.abs(k_diffs[:, 2])
-        gpar = np.sqrt(k_diffs[:, 0]**2 + k_diffs[:, 1]**2)
-        rc = 0.5 * self.rlat[2, 2]
-        factor = 1.0 
+    # def v2dt_vectorized(self, k_diffs):
+    #     # this function might be useful to avoid do loops
+    #     vc = self.dir_vol
+    #     vbz = 1.0 / (np.prod(self.ngrid) * vc)
+    #     gz = np.abs(k_diffs[:, 2])
+    #     gpar = np.sqrt(k_diffs[:, 0]**2 + k_diffs[:, 1]**2)
+    #     rc = 0.5 * self.rlat[2, 2]
+    #     factor = 1.0 
 
-        modk = self.modvec_vectorized(k_diffs)
+    #     modk = self.modvec_vectorized(k_diffs)
 
-        v2dt = np.zeros_like(gz)
+    #     v2dt = np.zeros_like(gz)
 
-        # Case where gpar and gz are below tolerance
-        mask_tol = (gpar < self.tolr) & (gz < self.tolr)
-        v2dt[mask_tol] = (vbz * 2 * self.pi) * (1/8.0 * rc * rc)
+    #     # Case where gpar and gz are below tolerance
+    #     mask_tol = (gpar < self.tolr) & (gz < self.tolr)
+    #     v2dt[mask_tol] = (vbz * 2 * self.pi) * (1/8.0 * rc * rc)
 
-        # Case where gpar is below tolerance and gz is above tolerance
-        mask_gpar_tol = (gpar < self.tolr) & (gz >= self.tolr)
-        v2dt[mask_gpar_tol] = (vbz * 2 * self.pi) * (factor / modk[mask_gpar_tol]**2) * (1.0 - np.cos(gz[mask_gpar_tol] * rc) - (gz[mask_gpar_tol] * rc * np.sin(gz[mask_gpar_tol] * rc)))
+    #     # Case where gpar is below tolerance and gz is above tolerance
+    #     mask_gpar_tol = (gpar < self.tolr) & (gz >= self.tolr)
+    #     v2dt[mask_gpar_tol] = (vbz * 2 * self.pi) * (factor / modk[mask_gpar_tol]**2) * (1.0 - np.cos(gz[mask_gpar_tol] * rc) - (gz[mask_gpar_tol] * rc * np.sin(gz[mask_gpar_tol] * rc)))
 
-        # General case
-        # ~ stands for NOT and | for OR in mask numpy arrays
-        mask_general = ~(mask_tol | mask_gpar_tol)
-        aux1 = gz[mask_general] / gpar[mask_general]
-        aux2 = gpar[mask_general] * rc
-        aux3 = gz[mask_general] * rc
-        aux4 = aux1 * np.sin(aux3)
-        aux5 = np.cos(aux3)
-        v2dt[mask_general] = (vbz * 2 * self.pi) * (factor / modk[mask_general]**2) * (1.0 + (np.exp(-aux2) * (aux4 - aux5)))
+    #     # General case
+    #     # ~ stands for NOT and | for OR in mask numpy arrays
+    #     mask_general = ~(mask_tol | mask_gpar_tol)
+    #     aux1 = gz[mask_general] / gpar[mask_general]
+    #     aux2 = gpar[mask_general] * rc
+    #     aux3 = gz[mask_general] * rc
+    #     aux4 = aux1 * np.sin(aux3)
+    #     aux5 = np.cos(aux3)
+    #     v2dt[mask_general] = (vbz * 2 * self.pi) * (factor / modk[mask_general]**2) * (1.0 + (np.exp(-aux2) * (aux4 - aux5)))
 
-        return v2dt * ha2ev
+    #     return v2dt * ha2ev
 
     def modvec_vectorized(self, k_diffs):
         # Vectorized modvec function 