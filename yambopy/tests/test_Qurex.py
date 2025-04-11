f8cde542da326755a910da3f493f22d8295be8a5
stevenbos123 <steventhijsbos98@gmail.com>
2025-04-11 16:39:08 +0200

    init TestTBWannier class

---
+++ yambopy/tests/test_Qurex.py
@@ -0,0 +1,16 @@
+from ..wannier import *
+
+
+
+class TestTBWannier():
+    def __init__(self):
+        pass
+
+    def grid_test():
+        pass
+    
+    def nnkp_test():
+        pass
+    
+    def h2p_test():
+        pass
