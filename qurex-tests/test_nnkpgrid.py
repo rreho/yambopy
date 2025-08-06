ad8f6238fb438690bb397bf15daa842fbdeb2340
Riccardo Reho riccardo.reho@uni.lu <rreho@access1.aion-cluster.uni.lux>
2025-08-06 19:13:59 +0200

    test update lower tolerances

--- qurex-tests/test_nnkpgrid.py
+++ qurex-tests/test_nnkpgrid.py
@@ -22,32 +22,31 @@ def test_kpb_grid():
     nnkp_kgrid, nnkp_qgrid = load_grids()
     result = nnkp_kgrid.kpb_grid_table
     reference = np.take_along_axis(nnkp_kgrid.nnkp, nnkp_kgrid.sort_idx[:,:,None], axis=1)
-    assert np.allclose(result, reference, atol=1e-8)
+    assert np.allclose(result, reference, atol=rtol=1e-6, atol=1e-7)
 
 def test_kmb_grid():
     '''k - b or k - B/2'''
     nnkp_kgrid, nnkp_qgrid = load_grids()
     result = nnkp_kgrid.kmb_grid
     reference = nnkp_kgrid.kpb_grid[:,::-1,:] # Because the b-vecs are sorted this works
-    assert np.allclose(result, reference, atol=1e-8)
+    assert np.allclose(result, reference, rtol=1e-6, atol=1e-7)
 
 def test_kmq_grid():
     '''k - q'''
     nnkp_kgrid, nnkp_qgrid = load_grids()
     result = nnkp_kgrid.kmq_grid
     reference = np.load("qurex-tests/data-tests/ref-data/nnkp_kmq_grid.npy")
-    assert np.allclose(result, reference, atol=1e-8)
+    assert np.allclose(result, reference, rtol=1e-6, atol=1e-7)
 
 def test_qmb_grid():
     '''q - B'''
     nnkp_kgrid, nnkp_qgrid = load_grids()
     result = nnkp_qgrid.qmb_grid
     reference = nnkp_qgrid.qpb_grid[:,::-1,:] # Because the b-vecs are sorted this works
-    assert np.allclose(result, reference, atol=1e-8)
-
+    assert np.allclose(result, reference, rtol=1e-6, atol=1e-7)
 def test_qpb_grid():
     '''q + B'''
     nnkp_kgrid, nnkp_qgrid = load_grids()
     result = nnkp_qgrid.qpb_grid_table
     reference = np.take_along_axis(nnkp_qgrid.nnkp, nnkp_qgrid.sort_idx[:,:,None], axis=1)
-    assert np.allclose(result, reference, atol=1e-8)
+    assert np.allclose(result, reference, rtol=1e-6, atol=1e-7)
