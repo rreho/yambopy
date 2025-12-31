c28b891f4caa82a66627319fc6ee1bd29b1872e9
Murali <muralidharrsvm7@gmail.com>
2025-12-31 06:44:41 +0530

    Edit doc

--- tutorial/databases_yambopy/total_crystal_angular_momentum_exciton.py
+++ tutorial/databases_yambopy/total_crystal_angular_momentum_exciton.py
@@ -26,12 +26,12 @@ path = '.'
 gw_bse_dir = 'GW_BSE'
 # BSE Job name. This is where your bse file are store after your yambo bse run.
 
-iexe = 1
-# This is the index of the specific exciton state you want to analyze.
+iexe = 0
+# This is the index of the specific exciton state you want to analyze (python indexing).
 # You likely found this index by looking at the absorption spectrum or energy table previously.
 
 degen_tol = 1e-2
-# This is the energy tolerance in electron-volts (eV).
+# This is the energy tolerance in eV.
 # States with energy differences smaller than this value are considered "degenerate" (the same energy)
 # and will be treated together during the angular momentum mixing.
 
@@ -45,18 +45,18 @@ lattice = YamboLatticeDB.from_db_file(os.path.join(path, 'SAVE', 'ns.db1'))
 # load exciton db
 filename = 'ndb.BS_diago_Q%d' % (iqpt)
 excdb = YamboExcitonDB.from_db_file(lattice, filename=filename,
-                                    folder=os.path.join(path, gw_bse_dir), neigs = iexe + 100)
+                                    folder=os.path.join(path, gw_bse_dir), neigs = iexe + 101)
 # We load the Exciton Database.
 # Critical Note: 'neigs' (number of eigenvalues) is set to 'iexe + 100'. 
 # We load more states than the target 'iexe' to ensure we capture all states that might be degenerate 
 # with our target. If you don't load the degenerate partners, the physics will be wrong.
 
 # Load the wavefunction database
-wfdb = YamboWFDB(path='.', latdb=lattice, bands_range=[np.min(excdb.table[:, 1]) - 1, np.max(excdb.table[:, 2])])
+wfdb = YamboWFDB(path=path, latdb=lattice, bands_range=[np.min(excdb.table[:, 1]) - 1, np.max(excdb.table[:, 2])])
 
 symm_mat_cart = lattice.sym_car[2] 
 # The rotation symmetry for which we want to find total_crys_angular_momentum values and simentineous eigenbasis
-# For example, here I am selecting 8th symmetry matrix which corresponds to 90 degree rotation along z.
+# For example, here I am selecting 3th symmetry matrix which corresponds to 120 degree rotation along z (I am doing this hBN).
 # you can check this in qe scf out file when verbosity = 'high'
 # Neverthesless you can also give your custom 3x3 matrix which must satisfy Rq = q and is a symmetry of the crystal.
 
@@ -67,22 +67,20 @@ frac_trans_cart = np.zeros(3)
 sbasis = excdb.total_crys_angular_momentum(wfdb, iexe, symm_mat_cart, frac_trans_cart, degen_tol=degen_tol)
 # This is the core calculation.
 # It computes the "Total Crystal Angular Momentum" for the target exciton ('iexe').
-# It uses the wavefunctions ('wfdb') and the specific symmetry ('symm_mat_cart').
-# 'np.zeros(3)' sets the center of rotation to the origin [0,0,0].
 # It returns 'sbasis', which contains the angular momentum values and the new basis vectors.
 
 jvals = sbasis[0]
 # These are the total crystal angular momentum eigenvalues (the 'j' values) for the degenerate states.
 
 Akcv_j = sbasis[1]
-# These are the new eigenvectors (Akcv) for the excitons i.e are simentineous eigenstates of Hbse and symmetry operator.
+# These are the new right eigenvectors (Akcv) for the excitons i.e are simentineous eigenstates of Hbse and symmetry operator.
 # i.e they represent how to mix the original states to create states with well-defined angular momentum.
 
 print('Total crystal angular momentum : ', jvals)
 # Prints the calculated j-values to the screen.
 
 ## Now we Plot read space wf
-idegen = np.array(excdb.get_degenerate(iexe+1, eps=degen_tol), dtype=int) - 1
+idegen = np.array(excdb.get_degenerate(iexe + 1, eps=degen_tol), dtype=int) - 1
 # We find the indices of all states that are degenerate with our target exciton.
 # 'iexe+1' is used because some internal functions use 1-based indexing, while python uses 0-based.
 # The result 'idegen' is an array of Python indices (0-based) for these states.
@@ -110,21 +108,18 @@ for i in idegen:
     # Now this state is energetically isolated and won't be averaged with others during plotting.
 
 # Now lets plot the real space wavefunction for each j value
-print('Computing real space wf for j = %.6f' % (jvals[0]))
-# We inform the user which j-value state is being generated.
-
-excdb.real_wf_to_cube(iexe=idegen[0], wfdb=wfdb, fixed_postion=[0.0, 0.0, 0.0], supercell=[9,9,9], degen_tol=1e-14, wfcCutoffRy=-1, fix_particle='e', phase=True)
-# This function generates the volumetric data file (.cube).
-# 'iexe=idegen[0]': We plot the first state of the degenerate group (corresponding to jvals[0]).
-# 'fixed_postion': We fix the position of one particle (usually the hole) at the origin.
-# 'fix_particle="e"': This is slightly counter-intuitive naming in some versions, but often implies fixing the electron to see the hole, or vice-versa. 
-# 'supercell=[9,9,9]': The plot will span a 9x9x9 grid of unit cells.
-# 'degen_tol=1e-14': We use a tiny tolerance here because we manually shifted the energies above; we want it to pick ONLY this one state.
-# 'wfcCutoffRy=-1': -1 means we use all G-vectors (no energy cutoff) for maximum precision.
+print("="*80)
+for i in range(len(jvals)):
+    print('*********** Plotting real space wf for state %d with j = %.2f ***************'
+          % (idegen[i] + 1, jvals[i]))
+    print("="*80)
+    excdb.real_wf_to_cube(iexe=idegen[i], wfdb=wfdb, fixed_postion=[0.0, 0.0, 0.0],
+                          supercell=[9,9,9], degen_tol=1e-14, wfcCutoffRy=-1,
+                          fix_particle='e', phase=True)
+    print("="*80)
 
 ## restore the db back
 excdb.eigenvalues[idegen] = old_eig_states_ene
 excdb.Akcv[idegen] = old_eig_states
 
 ## .cube will be dumped and use vesta to visualize it !
-# The script finishes here. You will find a .cube file in your directory which can be opened in VESTA.
